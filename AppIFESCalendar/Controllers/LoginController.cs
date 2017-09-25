182e6485cd136b1dd68bdac2507ce15c286cdfe3
calosguilherme <calosguilherme@hotmail.com>
2017-09-24 22:52:20 -0300

    LoginController
    LoginController + Model Usuário

---
+++ AppIFESCalendar/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class LoginController
+{
+    public ActionResult Logar(Usuario u){
+        if (nome == "admin" && senha == 123)
+        {
+            return RedirecionarAcao("Inicio");
+        }
+        else
+            return RedirecionarAcao("ErroLogin");
+}
+
+}
